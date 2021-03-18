440da8c96344939bc1f87f9e1c9b1eac4dfaa574
Robin East <robin.east@xense.co.uk>
2021-03-18 11:52:48 +0000

    add keymap param to extract_frames.py

--- extract_frames.py
+++ extract_frames.py
@@ -3,14 +3,14 @@ import cv2
 
 font = cv2.FONT_HERSHEY_SIMPLEX 
 
-def extract_frames(input_video, output_folder, exercise):
+def extract_frames(input_video, output_folder, exercise, keymap):
   video_name = input_video.split('/')[-1]
 
-  up_folder_path = f'{output_folder}/{exercise}_up'
-  down_folder_path = f'{output_folder}/{exercise}_down'
+  keys, suffixes = parse_keymap(keymap)
+  folder_map = {key:f'{output_folder}/{exercise}_{suffix}' for key, suffix in zip(keys,suffixes)}
 
-  check_and_create_dir(up_folder_path)
-  check_and_create_dir(down_folder_path)
+  for folder_path in folder_map.values():
+    check_and_create_dir(folder_path)
 
   video = cv2.VideoCapture(input_video)
 
@@ -30,17 +30,23 @@ def extract_frames(input_video, output_folder, exercise):
     print(key)
     if key & 0xFF == ord('q'):
       break
-    if key & 0xFF == ord('u'):
-      output_path = f'{up_folder_path}/{video_name}_{exercise}{cnt}.jpg'
+    if key & 0xFF in [k for k in keys]:
+      folder_path = folder_map[key & 0xFF]
+      output_path = f'{folder_path}/{video_name}_{exercise}{cnt}.jpg'
       print(f'save to {output_path}')
-      #frame = rotate_and_resize(frame, target_width=320) 
-      cv2.imwrite(output_path, frame)
-    if key & 0xFF == ord('d'):
-      output_path = f'{down_folder_path}/{video_name}_{exercise}{cnt}.jpg'
-      print(f'save to {output_path}')
-      #frame = rotate_and_resize(frame, target_width=320)
       cv2.imwrite(output_path, frame)
 
+def parse_keymap(keymap):
+  ''' Receives a string in <key>=<suffix>;<key>=<suffix>;... format
+      and returns a list of keys and suffixes. The key is a single letter
+      typed on the keyboard, the list of keys should be the ordinal value
+      of the key
+  '''
+  entries = keymap.split(';')
+  keys = [ord(entry.split('=')[0]) for entry in entries]
+  suffixes = [entry.split('=')[1] for entry in entries]
+  return keys, suffixes
+  
 def check_and_create_dir(path):
   ''' Check if folder path exists and create folder'''
   from pathlib import Path
@@ -64,8 +70,8 @@ def rotate_and_resize(img, target_width):
   dim = (target_width, target_height)
   print(f'resizing to {dim}')
   resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-  #rotated = cv2.rotate(resized, cv2.ROTATE_90_COUNTERCLOCKWISE)
-  #return rotated
+  rotated = cv2.rotate(resized, cv2.ROTATE_90_COUNTERCLOCKWISE)
+  return rotated
   return resized
 
 def parse_args():
@@ -73,11 +79,14 @@ def parse_args():
   parser.add_argument('input_video')
   parser.add_argument('output_folder')
   parser.add_argument('exercise')
+  parser.add_argument('--keymap', default='u=up;d=down', 
+      help='map keys to class suffxies in key=suffix format (suffix is alphanumeric only')
   return parser.parse_args()
 
 if __name__ == '__main__':
   args = parse_args()
+  print(f'Using keymap: {args.keymap}')
 
-  extract_frames(args.input_video, args.output_folder, args.exercise)
+  extract_frames(args.input_video, args.output_folder, args.exercise, args.keymap)
 
 
