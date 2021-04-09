c206bc4ca8118ed09724b36a6a7fbb42578c45cb
Robin East <robin.east@xense.co.uk>
2021-04-09 12:55:22 +0100

    add bootstrap_in and csvs_out command line parameters

--- automatic_filtration.py
+++ automatic_filtration.py
@@ -1,3 +1,4 @@
+import argparse
 from bootstrap_helper import BootstrapHelper
 from pose_embedder import FullBodyPoseEmbedder
 from pose_classifier import PoseClassifier
@@ -14,40 +15,58 @@ from pose_classifier import PoseClassifier
 #       image_002.jpg
 #       ...
 #     ...
-bootstrap_images_in_folder = 'fitness_poses_images_in'
+BOOTSTRAP_IMAGES_IN_FOLDER_DEFAULT = 'fitness_poses_images_in'
 
 # Output folders for bootstrapped images and CSVs.
 bootstrap_images_out_folder = 'fitness_poses_images_out'
-bootstrap_csvs_out_folder = 'fitness_poses_csvs_out'
+BOOTSTRAP_CSVS_OUT_FOLDER_DEFAULT = 'fitness_poses_csvs_out'
 
-# Initialize helper.
-bootstrap_helper = BootstrapHelper(
-    images_in_folder=bootstrap_images_in_folder,
-    images_out_folder=bootstrap_images_out_folder,
-    csvs_out_folder=bootstrap_csvs_out_folder,
-)
+def filtration(bootstrap_images_in_folder, bootstrap_csvs_out_folder):
+  # Initialize helper.
+  bootstrap_helper = BootstrapHelper(
+      images_in_folder=bootstrap_images_in_folder,
+      images_out_folder=bootstrap_images_out_folder,
+      csvs_out_folder=bootstrap_csvs_out_folder,
+  )
+  
 
+  # Find outliers.
+  
+  # Transforms pose landmarks into embedding.
+  pose_embedder = FullBodyPoseEmbedder()
+  
+  # Classifies give pose against database of poses.
+  pose_classifier = PoseClassifier(
+      pose_samples_folder=bootstrap_csvs_out_folder,
+      pose_embedder=pose_embedder,
+      top_n_by_max_distance=30,
+      top_n_by_mean_distance=10)
+  
+  outliers = pose_classifier.find_pose_sample_outliers()
+  print('Number of outliers: ', len(outliers))
+  
+  bootstrap_helper.analyze_outliers(outliers)
+  
+  # Remove all outliers (if you don't want to manually pick).
+  bootstrap_helper.remove_outliers(outliers)
+  
+  # Align CSVs with images after removing outliers.
+  bootstrap_helper.align_images_and_csvs(print_removed_items=False)
+  bootstrap_helper.print_images_out_statistics()
 
-# Find outliers.
+def parse_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument(	'--bootstrap_in', 
+			default=BOOTSTRAP_IMAGES_IN_FOLDER_DEFAULT,
+			help='')
+  parser.add_argument(	'--csvs_out',
+			default=BOOTSTRAP_CSVS_OUT_FOLDER_DEFAULT,
+			help='')
+  return parser.parse_args()
 
-# Transforms pose landmarks into embedding.
-pose_embedder = FullBodyPoseEmbedder()
+if __name__ == '__main__':
+  args = parse_args()
+  
+  print(args)
 
-# Classifies give pose against database of poses.
-pose_classifier = PoseClassifier(
-    pose_samples_folder=bootstrap_csvs_out_folder,
-    pose_embedder=pose_embedder,
-    top_n_by_max_distance=30,
-    top_n_by_mean_distance=10)
-
-outliers = pose_classifier.find_pose_sample_outliers()
-print('Number of outliers: ', len(outliers))
-
-bootstrap_helper.analyze_outliers(outliers)
-
-# Remove all outliers (if you don't want to manually pick).
-bootstrap_helper.remove_outliers(outliers)
-
-# Align CSVs with images after removing outliers.
-bootstrap_helper.align_images_and_csvs(print_removed_items=False)
-bootstrap_helper.print_images_out_statistics()
+  filtration(args.bootstrap_in, args.csvs_out)
--- bootstrap.py
+++ bootstrap.py
@@ -1,5 +1,6 @@
 # Code based on the Colab https://colab.research.google.com/drive/19txHpN8exWhstO6WVkfmYYVC6uug_oVR#scrollTo=bERVPO8Ja6j7
 
+import argparse
 from bootstrap_helper import BootstrapHelper
 
 # Required structure of the images_in_folder:
@@ -14,33 +15,47 @@ from bootstrap_helper import BootstrapHelper
 #       image_002.jpg
 #       ...
 #     ...
-bootstrap_images_in_folder = 'fitness_poses_images_in'
+BOOTSTRAP_IMAGES_IN_FOLDER_DEFAULT = 'fitness_poses_images_in'
 
 # Output folders for bootstrapped images and CSVs.
 bootstrap_images_out_folder = 'fitness_poses_images_out'
-bootstrap_csvs_out_folder = 'fitness_poses_csvs_out'
+BOOTSTRAP_CSVS_OUT_FOLDER_DEFAULT = 'fitness_poses_csvs_out'
 
-# Initialize helper.
-bootstrap_helper = BootstrapHelper(
-    images_in_folder=bootstrap_images_in_folder,
-    images_out_folder=bootstrap_images_out_folder,
-    csvs_out_folder=bootstrap_csvs_out_folder,
-)
+def bootstrap(bootstrap_images_in_folder, bootstrap_csvs_out_folder):
+  # Initialize helper.
+  bootstrap_helper = BootstrapHelper(
+      images_in_folder=bootstrap_images_in_folder,
+      images_out_folder=bootstrap_images_out_folder,
+      csvs_out_folder=bootstrap_csvs_out_folder,
+  )
+  
+  # Check how many pose classes and images for them are available.
+  bootstrap_helper.print_images_in_statistics()
+  
+  # Bootstrap all images.
+  # Set limit to some small number for debug.
+  bootstrap_helper.bootstrap(per_pose_class_limit=None)
+  
+  # Check how many images were bootstrapped.
+  bootstrap_helper.print_images_out_statistics()
+  
+  # After initial bootstrapping images without detected poses were still saved in
+  # the folderd (but not in the CSVs) for debug purpose. Let's remove them.
+  bootstrap_helper.align_images_and_csvs(print_removed_items=False)
+  bootstrap_helper.print_images_out_statistics()
+  
+def parse_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument(	'--bootstrap_in', 
+			default=BOOTSTRAP_IMAGES_IN_FOLDER_DEFAULT,
+			help='location of source videos')  
+  parser.add_argument(	'--csvs_out',
+			default=BOOTSTRAP_CSVS_OUT_FOLDER_DEFAULT,
+			help='location of output CSVs')
+  return parser.parse_args()
 
-# Check how many pose classes and images for them are available.
-bootstrap_helper.print_images_in_statistics()
+if __name__ == '__main__':
+  args = parse_args()
+  print(args)
 
-# Bootstrap all images.
-# Set limit to some small number for debug.
-bootstrap_helper.bootstrap(per_pose_class_limit=None)
-
-# Check how many images were bootstrapped.
-bootstrap_helper.print_images_out_statistics()
-
-# After initial bootstrapping images without detected poses were still saved in
-# the folderd (but not in the CSVs) for debug purpose. Let's remove them.
-bootstrap_helper.align_images_and_csvs(print_removed_items=False)
-bootstrap_helper.print_images_out_statistics()
-
-
-
+  bootstrap(args.bootstrap_in, args.csvs_out)
